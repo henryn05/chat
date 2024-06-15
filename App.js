961373c4ad888bf2f4c910361958bc4953d970d0
Henry Nguyen <belugas2005@gmail.com>
2024-06-15 09:58:30 -0500

    Initialized Firebase and Firestore

--- App.js
+++ App.js
@@ -4,9 +4,20 @@ import Chat from "./components/Chat";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  const firebaseConfig = {
+
+  };
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get reference to service
+  const db = getFirestore(app);
 
   return (
     <NavigationContainer>
@@ -17,10 +28,12 @@ const App = () => {
           name="Start"
           component={Start}
         />
-        <Stack.Screen
-          name="Chat"
-          component={Chat}
-        />
+        <Stack.Screen name="Chat">
+          {/*
+            Passes db collection into chat component
+          */}
+          {props => <Chat db={db} {...props}/>}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   )
